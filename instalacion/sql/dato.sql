8ff52d0316f57e5628fef16cada98229b4a0b752
jonathanlopezb <asuntosantarticos3@cco.gov.co>
2023-08-18 11:01:51 -0500

    db config usr dat

---
+++ instalacion/sql/dato.sql
@@ -0,0 +1,21 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = public, pg_catalog;
+
+--
+-- Data for Name: usuario; Type: TABLE DATA; Schema: public; Owner: orfeo_usr
+--
+
+INSERT INTO usuario VALUES (1, '1', 'ADMON', '2019-11-05', '1232f297a57a5a743894a0e4a8', '1', 'Usuario de Soporte', '1', '1', '1', '1234567890', 5, '210428075608o1921688236ADMON', '2021-04-28', NULL, NULL, 'soporte.skinatech@gmail.com', NULL, NULL, 0, 2, 1, 1, NULL, '1', NULL, 3, 3, 3, 1, 1, 2, NULL, NULL, NULL, 2, NULL, 1, 2, 0, 1, 1, 1, 0, 2, '--', 170, 1, 0, '1', '1', 0, 1, 0, 0, 1, 0, 1, 3, 0, 2, 1, 1, 3, 1, 1, 1, 1, 1, 1, 0, 0);
+
+--
+-- PostgreSQL database dump complete
+--
